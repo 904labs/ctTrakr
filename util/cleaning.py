b7fd9ceb22ecee5bdc8934cefa5ff23e80edf6af
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-02-25 12:20:24 +0100

    Cleaning for python 3

--- util/cleaning.py
+++ util/cleaning.py
@@ -1,16 +1,20 @@
 
-from HTMLParser import HTMLParser
+# Import for python 3, else 2.7
+try:
+  from html.parser import HTMLParser
+except ImportError:
+  from HTMLParser import HTMLParser
 
-# Strip xml/html markup
+
 class Clean(HTMLParser):
-  def __init__(self):
-    self.reset()
-    self.strict = False
-    self.convert_charrefs= True
-    self.fed = []
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
 
-  def handle_data(self, d):
-    self.fed.append(d)
+    def handle_data(self, d):
+        self.fed.append(d)
 
-  def get_data(self):
-    return ''.join(self.fed)
+    def get_data(self):
+        return ''.join(self.fed)
